505365199c6133f7e5f1389f9c9b04f23ded65e8
Ian 'Uther' Lawson <ian.lawson@redhat.com>
2015-10-13 17:15:46 +0100

    I hate Maven. It is complicated in a pointless way. It is too full of itself. It is just a jumped up Ant with delusions of Eclipse like grandeur. It is like a bit-player on a retarded version of the Muppets.

---
+++ src/main/java/org/uth/amq/tests/ExampleProducer.java
@@ -0,0 +1,58 @@
+package org.uth.amq.tests;
+
+import java.util.Properties;
+import javax.jms.*;
+import javax.naming.*;
+
+public class ExampleProducer
+{
+  private String queueName = "org.uth.testqueue1";
+  private String user = "uther";
+  private String password = "Pendragon1";
+  
+  private String url = "tcp://hostname:61616";
+	
+  private boolean transacted;
+  private boolean isRunning = false;
+	
+  public static void main(String[] args) throws NamingException, JMSException
+  {
+    ExampleProducer producer = new ExampleProducer();
+    producer.run();
+  }
+	
+  public ExampleProducer() {}
+	
+  public void run() throws NamingException, JMSException
+  {		
+    //JNDI properties
+    Properties props = new Properties();
+    props.setProperty(Context.INITIAL_CONTEXT_FACTORY, "org.apache.activemq.jndi.ActiveMQInitialContextFactory");
+    props.setProperty(Context.PROVIDER_URL, url);
+		
+    //specify queue propertyname as queue.jndiname
+    props.setProperty("queue.slQueue", queueName);
+		
+    javax.naming.Context ctx = new InitialContext(props);
+    ConnectionFactory connectionFactory = (ConnectionFactory)ctx.lookup("ConnectionFactory");
+    Connection connection = connectionFactory.createConnection(user, password);
+    connection.start();
+		
+    Session session = connection.createSession(transacted, Session.AUTO_ACKNOWLEDGE);
+		
+    Destination destination = (Destination)ctx.lookup("slQueue");
+    MessageProducer producer = session.createProducer(destination);
+    producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
+
+    for( int loop = 0; loop < 100; loop++ )
+    {
+      TextMessage message = session.createTextMessage("Test Message " + loop);
+      producer.send(message);
+    }
+		
+    producer.close();
+    session.close();
+    connection.close();
+  }
+}
+
